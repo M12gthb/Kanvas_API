779e1aba6e9c5767e1c733c6a779e6f82437c643
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2025-04-25 14:23:17 -0300

    raw: add new settings

--- _core/settings.py
+++ _core/settings.py
@@ -24,6 +24,26 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# project_base/settings.py
+if os.environ.get("TEST"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_DB"),
+            "USER": os.environ.get("POSTGRES_USER"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+            "HOST": "db",
+            "PORT": 5432,
+        }
+    }
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
